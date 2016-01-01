7e4343a3582b9c63005b1635ddc3c027646691d9
darka91 <andrewinci@gmail.com>
2016-01-01 14:10:40 +0100

    Add certificate part
    + Certificate for test
    + Certificate message functions

--- include/HandshakeMessages/Certificate.h
+++ include/HandshakeMessages/Certificate.h
@@ -5,10 +5,57 @@
 //  Created on 28/12/15.
 //  Copyright © 2015 Mello, Darka. All rights reserved.
 //
+//  The certificate message is divided in this way:
+//  lenght of all message: 3 byte
+//  foreach certificate:
+//  length of the current certificate : 3 byte
+//  certificate...
+//
+//  In total we have 3 + 3*NumCertificate + the lenght of all certificate
 
 #ifndef Certificate_h
 #define Certificate_h
 
 #include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
+#include <openssl/x509.h>
+#include <openssl/pem.h>
+
+#define REV16(value)({(value & 0x00FFU) << 8 | (value & 0xFF00U) >> 8;})
+#define REV32(value)({(value & 0x000000FFU) << 24 | (value & 0x0000FF00U) << 8 |(value & 0x00FF0000U) >> 8 | (value & 0xFF000000U) >> 24;})
 #endif /* Certificate_h */
+
+typedef struct DER_certificate_t{
+    X509 *X509_certificate;
+    struct DER_certificate_t *next;
+}DER_certificate;
+
+typedef struct {
+    uint32_t cert_length; //only 3 byte are used
+    DER_certificate *certificate_list;
+}certificate_message;
+
+/*
+ * Create a certificate message from a list of certificate
+ * cert_files_name  : list of path for certificates in DER format
+ * list_size        : the number of path in cert_files_name
+ *  return a certificate_message ready for serialize and send
+ */
+certificate_message *make_certificate_message(char **cert_files_name, int list_size);
+
+/*
+ * Serialization of the packet like always
+ */
+void serialize_certificate_message(certificate_message *cert, unsigned char **stream, uint32_t *len);
+
+/*
+ * Deserialization
+ */
+certificate_message *deserialize_certificate_message(unsigned char *stream, uint32_t len);
+
+/*
+ * Free the allocation
+ */
+void free_certificate_message(certificate_message *cert);
--- src/HandshakeMessages/Certificate.c
+++ src/HandshakeMessages/Certificate.c
@@ -6,4 +6,133 @@
 //  Copyright © 2015 Mello, Darka. All rights reserved.
 //
 
+#ifdef MAKEFILE
 #include "HandshakeMessages/Certificate.h"
+#else
+#include "Certificate.h"
+#endif
+
+
+certificate_message *make_certificate_message(char **cert_files_name, int list_size){
+    
+    certificate_message *result = malloc(sizeof(certificate_message));
+    result->cert_length = 0;
+    result->certificate_list = NULL;
+    for(int i=0;i<list_size;i++){
+        char *file_name = cert_files_name[i];
+        X509 *x=NULL;
+        FILE *fp = fopen(file_name, "r");
+        
+        if (!fp) {
+            fprintf(stderr, "unable to open: %s\n", file_name);
+            exit(-1);
+        }
+        
+        x = PEM_read_X509(fp, NULL, NULL, NULL);
+        if (!x) {
+            fprintf(stderr, "unable to parse certificate in: %s\n", file_name);
+            fclose(fp);
+            exit(-1);
+        }
+        //free with X509_free(*)
+        fclose(fp);
+        
+        //create DER_certificate node
+        DER_certificate *cert_node = malloc(sizeof(DER_certificate));
+        cert_node->next = NULL;
+        cert_node->X509_certificate = x;
+
+        //append the new certificate to the list
+        DER_certificate *last = result->certificate_list;
+        if(last == NULL){
+            //no node in the list
+            result->certificate_list = cert_node;
+        }
+        else{
+            while (last->next != NULL)
+                last = last->next;
+            last->next = cert_node;
+        }
+        int raw_certificate_len = i2d_X509(cert_node->X509_certificate, NULL);
+        result->cert_length += (raw_certificate_len+3); // the length of the certificate plus the length field
+    }
+    return result;
+}
+
+void serialize_certificate_message(certificate_message *cert, unsigned char **stream, uint32_t *len){
+    *len = cert->cert_length+3;
+    *stream = malloc(*len); //3 byte for the lenght of all certificate stream
+    unsigned char *buff=*stream;
+    
+    //copy length of the entire message
+    uint32_t len_t = REV32(cert->cert_length)>>8;
+    memcpy(buff, &len_t, 3);
+    buff+=3;
+    
+    //copy all certificates
+    DER_certificate *node = cert->certificate_list;
+    
+    while (node!=NULL) {
+        //get certificate
+        unsigned char *raw_cert=NULL;
+        int raw_certificate_len = i2d_X509(node->X509_certificate, &raw_cert);
+        if(raw_certificate_len>0){
+            
+            //copy length
+            len_t = REV32(raw_certificate_len)>>8;
+            memcpy(buff, &len_t, 3);
+            buff+=3;
+            
+            //copy the certificate
+            memcpy(buff, raw_cert, raw_certificate_len);
+            buff+=raw_certificate_len;
+        }
+        node = node->next;
+    }
+}
+
+certificate_message *deserialize_certificate_message(unsigned char *stream, uint32_t len){
+    certificate_message *result = malloc(sizeof(certificate_message));
+    result->certificate_list = NULL;
+    result->cert_length = len - 3; //since 3 byte are used for the lenght
+    unsigned char *buff = stream;
+    buff+=3; //we "ignore" the first 3 byte since we already know the length of the entire packet from the stream len
+    uint32_t len_t;
+    while (buff<(stream+len-3)) {
+        DER_certificate *certificate = malloc(sizeof(DER_certificate));
+        certificate->next = NULL;
+        
+        //certficate length
+        memcpy(&len_t,buff, 3);
+        len_t = REV32(len_t)>>8;
+        buff+=3;
+        
+        //build X509
+        if(!d2i_X509(&certificate->X509_certificate, (const unsigned char **)&buff, len_t)){
+            fprintf(stderr, "\nError in deserialize certificate\n");
+            exit(-1);
+        }
+        if(result->certificate_list == NULL)
+            result->certificate_list = certificate;
+        else{
+            DER_certificate *last = result->certificate_list;
+            while (last->next != NULL)
+                last = last->next;
+            last->next = certificate;
+        }
+    }
+    return result;
+}
+
+void free_certificate_message(certificate_message *cert){
+    DER_certificate *node = cert->certificate_list;
+    
+    //clean the certificate list
+    while (node!=NULL) {
+        DER_certificate *next = node->next;
+        X509_free(node->X509_certificate);
+        free(node);
+        node = next;
+    }
+    free(cert);
+}
