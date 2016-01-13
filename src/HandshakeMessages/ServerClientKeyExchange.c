b6a1e7ea39cea0c0b54e75d4925b9786a02103ad
darka91 <andrewinci@gmail.com>
2016-01-13 23:11:08 +0100

    Temp commit

--- src/HandshakeMessages/ServerClientKeyExchange.c
+++ src/HandshakeMessages/ServerClientKeyExchange.c
@@ -12,56 +12,107 @@
 #include "ServerClientKeyExchange.h"
 #endif
 
-void serialize_key_exchange(uint32_t key_length, unsigned char *encrypted_premaster_key, unsigned char **stream, uint32_t *len, key_exchange_algorithm kx){
+void serialize_client_key_exchange(void *server_key_exchange, unsigned char **stream, uint32_t *streamLen, key_exchange_algorithm kx){
     if(kx == RSA_KX){
+        RSA_server_key_exchange *rsa_server_key_ex =(RSA_server_key_exchange*)server_key_exchange;
         //the first 3 message byte are the key length
-        unsigned char *buff = malloc(key_length+2);
+        unsigned char *buff = malloc(rsa_server_key_ex->key_length+2);
         *stream = buff;
         //add lenght
-        uint32_t temp = REV32(key_length)>>8;
+        uint32_t temp = REV32(rsa_server_key_ex->key_length)>>8;
         memcpy(buff, &temp, 3);
         buff+=3;
         //add key
-        memcpy(buff, encrypted_premaster_key, key_length);
-        *len=key_length+3;
+        memcpy(buff, rsa_server_key_ex->encrypted_premaster_key, rsa_server_key_ex->key_length);
+        *streamLen=rsa_server_key_ex->key_length+3;
+    }else if(kx == DHE_RSA_KX){
+        DH_server_key_exchange *dh_server_key_ex = (DH_server_key_exchange*)server_key_exchange;
+        unsigned char *buf;
+        uint16_t len;
+        int pLen = BN_num_bytes(dh_server_key_ex->p), gLen = BN_num_bytes(dh_server_key_ex->g), pubKeyLen = BN_num_bytes(dh_server_key_ex->pubKey);
+        
+        *streamLen = 2+pLen+2+gLen+2+pubKeyLen+2+2+dh_server_key_ex->signature_length;
+       
+        buf = malloc(*streamLen);
+        *stream = buf;
+        pLen = REV16(pLen);
+        memcpy(buf, &pLen, 2);
+        buf+=2;
+        len = BN_bn2bin(dh_server_key_ex->p, buf);
+        buf+=len;
+        
+        gLen = REV16(gLen);
+        memcpy(buf, &gLen, 2);
+        buf+=2;
+        
+        len = BN_bn2bin(dh_server_key_ex->g, buf);
+        buf+=len;
+        
+        pubKeyLen = REV16(pubKeyLen);
+        memcpy(buf, &pubKeyLen, 2);
+        buf+=2;
+        
+        len = BN_bn2bin(dh_server_key_ex->pubKey, buf); //copy public key
+        buf+=len;
+        
+        memcpy(buf, &(dh_server_key_ex->sign_hash_alg), 2); //copy hash, sign alg
+        buf+=2;
+        
+        len = dh_server_key_ex->signature_length;
+        len = REV16(len);
+        memcpy(buf, &len, 2);
+        buf+=2;
+        
+        memcpy(buf, dh_server_key_ex->signature, dh_server_key_ex->signature_length);
     }
 }
 
-void deserialize_key_exchange(uint32_t message_len, unsigned char *message, unsigned char **encrypted_premaster_key, uint32_t *key_len, key_exchange_algorithm kx){
+void *deserialize_client_key_exchange(uint32_t message_len, unsigned char *message, key_exchange_algorithm kx){
     if(kx == RSA_KX){
-        memcpy(key_len, message, 3);
+        RSA_server_key_exchange *rsa_server_key_ex = malloc(sizeof(RSA_server_key_exchange));
+        memcpy(&(rsa_server_key_ex->key_length), message, 3);
         message+=3;
         
-        *key_len = REV32(*key_len)>>8;
-        unsigned char *buff = malloc(*key_len);
-        *encrypted_premaster_key = buff;
-        memcpy(buff, message, *key_len);
+        rsa_server_key_ex->key_length = REV32(rsa_server_key_ex->key_length)>>8;
+        unsigned char *buff = malloc(rsa_server_key_ex->key_length);
+        rsa_server_key_ex->encrypted_premaster_key = buff;
+        memcpy(buff, message, rsa_server_key_ex->key_length);
+        
+        return rsa_server_key_ex;
+    }else if(kx == DHE_RSA_KX){
+        DH_server_key_exchange *dh_server_key_ex = malloc(sizeof(DH_server_key_exchange));
+        uint16_t len;
+        memcpy(&len, message, 2);
+        message+=2;
+        len = REV16(len);
+        if (BN_bin2bn(message, len, dh_server_key_ex->p)==NULL)
+            printf("Error in deserialize dh_server_key_ex server params (binary to big number, p)");
+        message+=len;
+        memcpy(&len, message, 2);
+        message+=2;
+        len = REV16(len);
+        if (BN_bin2bn(message, len, dh_server_key_ex->g)==NULL)
+            printf("Error in deserialize dh_server_key_ex server params (binary to big number, g)");
+        message+=len;
+        memcpy(&len, message, 2);
+        message+=2;
+        len = REV16(len);
+        if (BN_bin2bn(message, len, dh_server_key_ex->pubKey)==NULL)
+            printf("Error in deserialize dh_server_key_ex server params (binary to big number, pubKey)");
+        message+=len;
+        memcpy(&(dh_server_key_ex->sign_hash_alg), message, 2);
+        message+=2;
+        memcpy(&(dh_server_key_ex->signature_length), message, 2);
+        message+=2;
+        memcpy(dh_server_key_ex->signature, message, dh_server_key_ex->signature_length);
+        message+=2;
     }
+    return NULL;
 }
 
-void PRF(const EVP_MD *hash, unsigned char *secret, int secret_len, char *label, unsigned char *seed, int seed_len, int result_len, unsigned char **result){
-    int buffer_size = (hash->md_size);
-    unsigned char *buff = malloc(buffer_size);
-    int label_len = (int)strlen(label);
-    *result = buff;
-    
-    //compute p_hash(secret,seed)
-    //secret is equal to secret
-    //seed is equal to label concatenate with seed
-    unsigned char *seed_p = malloc(label_len+seed_len);
-    memcpy(seed_p, label, label_len);
-    memcpy(seed_p+label_len, seed, seed_len);
-    
-    //compute A_i
-    int tot_len = 0;
-    unsigned int a_len = label_len+seed_len;
-    unsigned char *a = seed_p;
-    while (tot_len<result_len) {
-        unsigned char *temp = NULL;
-        temp = HMAC(hash, secret, secret_len, a, a_len, NULL, &a_len);
-        a = temp;
-        memcpy(buff+tot_len, a, a_len);
-        tot_len+=a_len;
-    }
-    free(seed_p);
+void free_DH_server_key_exchange(DH_server_key_exchange *params){
+    BN_free(params->g);
+    BN_free(params->p);
+    BN_free(params->pubKey);
+    free(params->signature);
 }
